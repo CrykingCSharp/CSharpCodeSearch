c08db9ad95d4e9d157ead0c5e2c77ac4bc8859ad
cryking.Guo <cryking.guo@outlook.com>
2018-10-29 21:12:27 +0800

    CodeSearch in PC

---
+++ CodeSearch/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace CodeSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_codefile = new System.Windows.Forms.ListBox();
+            this.lab1 = new System.Windows.Forms.Label();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lb_codefile
+            // 
+            this.lb_codefile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lb_codefile.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lb_codefile.FormattingEnabled = true;
+            this.lb_codefile.ItemHeight = 12;
+            this.lb_codefile.Location = new System.Drawing.Point(35, 43);
+            this.lb_codefile.Name = "lb_codefile";
+            this.lb_codefile.Size = new System.Drawing.Size(203, 292);
+            this.lb_codefile.TabIndex = 0;
+            this.lb_codefile.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Lb_codefile_MouseClick);
+            // 
+            // lab1
+            // 
+            this.lab1.AutoSize = true;
+            this.lab1.Location = new System.Drawing.Point(33, 29);
+            this.lab1.Name = "lab1";
+            this.lab1.Size = new System.Drawing.Size(53, 12);
+            this.lab1.TabIndex = 1;
+            this.lab1.Text = "文件列表";
+            // 
+            // tb1
+            // 
+            this.tb1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb1.Location = new System.Drawing.Point(255, 43);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(229, 21);
+            this.tb1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(490, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 21);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "搜索代码";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tb2
+            // 
+            this.tb2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tb2.ForeColor = System.Drawing.Color.Red;
+            this.tb2.Location = new System.Drawing.Point(0, 0);
+            this.tb2.Multiline = true;
+            this.tb2.Name = "tb2";
+            this.tb2.ReadOnly = true;
+            this.tb2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tb2.Size = new System.Drawing.Size(343, 263);
+            this.tb2.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.tb2);
+            this.panel1.Location = new System.Drawing.Point(245, 79);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(343, 263);
+            this.panel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 344);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.lab1);
+            this.Controls.Add(this.lb_codefile);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "源码搜索";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lb_codefile;
+        private System.Windows.Forms.Label lab1;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ CodeSearch/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CodeSearch
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> dt = new Dictionary<string, string>();
+        object slock = new object();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int i = 0;
+        int iSearchCnt = 0,iTotalDrivers=0;//搜索了的盘符个数 总盘符数
+        void FileSearch(string driver)
+        {
+            string s = driver;
+            
+            {
+                DriveInfo di = new DriveInfo(s);
+                Console.WriteLine(di.DriveType);
+                if (false==di.IsReady)
+                {
+                    return;
+                }
+                try
+                {
+                    string[] ast = Directory.GetFiles(s, "*.cs");
+                    if (ast.Length == 0)
+                        ast = Directory.GetDirectories(s);
+                    else
+                    {
+                        foreach (string s2 in ast)
+                        {
+                            string t2 = s2.Substring(s2.LastIndexOf('\\') + 1);
+                            //lb_codefile.Items.Add(t2);
+                            //dt.Add(i.ToString() + t2, s2);
+                            
+                            lock (slock)
+                            {
+                                AddToListBox(i, t2, s2);
+                                i++;
+                            }
+                        }
+                    }
+                    foreach (string s1 in ast)
+                    {
+                        tb2.Invoke(new Action(() =>
+                        {
+                            tb2.Text = string.Format("正在搜索:{0}",s1);
+                        }));
+                        try
+                        {
+                            if (s1.Contains(@"Program Files") || s1.Contains(@"C:\Windows")
+                                || s1.Contains(@"C:\Python33") || s1.Contains(@"D:\cygwin64")
+                                || s1.Contains("System Volume Information")) continue;
+                            string[] atmp = Directory.GetFiles(s1, "*.cs", SearchOption.AllDirectories);
+                            if (atmp.Length != 0)
+                            {
+                                foreach (string s2 in atmp)
+                                {
+                                    string t2 = s2.Substring(s2.LastIndexOf('\\') + 1);
+                                    //lb_codefile.Items.Add(t2);
+                                    //dt.Add(i.ToString() + t2, s2);
+                                    AddToListBox(i, t2, s2);
+                                    lock (slock)
+                                    {
+                                        i++;
+                                    }
+                                }
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                            continue;
+                        }
+                        if (lab1.InvokeRequired&&i>0)
+                        {
+                            lab1.Invoke(new Action<int>((c) =>
+                            {
+                                lab1.Text = string.Format(":.CS文件数({0})", c);
+                            }),i);
+                        }
+                    }
+                    iSearchCnt++;
+                    if (iSearchCnt == iTotalDrivers-1)
+                    {
+                        tb2.Invoke(new Action(() =>
+                        {
+                            tb2.Text = "搜索完成";
+                        }));
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+            }
+        }
+
+        void AddToListBox(int i,string file,string path)
+        {
+            Invoke(new Action(() =>
+            {
+                lb_codefile.Items.Add(file);
+                dt.Add(i.ToString() + file, path);
+                //lb_codefile.Refresh();
+            }));
+        }
+
+        bool bSearch = false;
+        List<string> sFiles = new List<string>();//已搜索的包含了代码的文件集合
+        int iCnt = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tb1.Text.Trim())) return;
+
+            int iFileCount = lb_codefile.Items.Count;
+
+            for(iCnt = 0; iCnt < iFileCount; iCnt = iCnt + 10)
+            {
+                string s = SelectFile(iCnt, 10 + iCnt);
+                if (s.Length > 0)
+                {
+                    sFiles.Add(s);
+                    break;
+                }
+            }
+            if (false == bSearch)
+            {
+                //本地搜索不到,去网上搜索
+                System.Diagnostics.Process.Start(@"IEXPLORE.EXE", @"http://cn.bing.com/search?q=" + tb1.Text);
+            }
+        }
+
+        string SelectFile(int s,int e)
+        {
+            string res = "";
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = s; i < e; i++)
+            {
+                string strfile = dt[i + lb_codefile.Items[i].ToString()].ToString();
+                sb.Clear();
+                if (sFiles.Contains(strfile))
+                {
+                    continue;
+                }
+                if(bSearch==false)
+                { 
+                using (StreamReader sr = new StreamReader(strfile, Encoding.Default))
+                {
+                    sb.Append(sr.ReadToEndAsync());
+                    if (sb.ToString().Contains(tb1.Text))
+                    {
+                        tb2.Text = lb_codefile.Items[i].ToString() + "\r\n\r\n" + sb.ToString();
+                        bSearch = true;
+                        res=strfile;
+                        break;
+                    }
+                }
+                }
+            }
+
+            return res;
+        } 
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] astr = Directory.GetLogicalDrives();
+            iTotalDrivers = astr.Length;
+            for (int i = 0; i < iTotalDrivers; i++)
+            {
+                Task t = new Task(drive => FileSearch((string)drive), astr[i]);
+                t.Start();
+            }
+            tb2.Text = "正在扫描电脑中所有.cs文件,请等待...";
+            //Task.Run(()=> FileSearch());
+        }
+
+        private void Lb_codefile_MouseClick(object sender, MouseEventArgs e)
+        {
+            ListBox lb1 = (ListBox)sender;
+            if (lb1.SelectedIndex > 0)
+            {
+                string strfile = dt[lb1.SelectedIndex.ToString() + lb1.Text].ToString();
+                StringBuilder sb = new StringBuilder(5000);
+                try
+                {
+                    using (StreamReader sr = new StreamReader(strfile, Encoding.Default))
+                    {
+                        sb.Append(sr.ReadToEnd());
+                    }
+                    tb2.Text = string.Format("文件路径{0}{1}{2}", strfile, Environment.NewLine, sb.ToString());
+                }
+                catch
+                {
+                    tb2.Text = "未找到文件";
+                }
+            }
+        }
+    }
+}
